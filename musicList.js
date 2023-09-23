ce4ea1b01eeedc9318c2c043bc75898d045cf185
sanjay9303 <145863321+sanjay9303@users.noreply.github.com>
2023-09-23 22:39:11 +0530

    Add files via upload

---
+++ musicList.js
@@ -0,0 +1,49 @@
+const allMusic = [
+  {
+    src: 'song1',
+    bg: 'images/images.png',
+    name: 'Humare saath jab raghunath to'
+  },
+  {
+    src: 'song3',
+    bg: 'images/images.png',
+    name: 'Bawli hai ke'
+  },
+  {
+    src: 'song4',
+    bg: 'images/images.png',
+    name: 'Hum Toh Deewane Yasser Desai'
+  },
+  {
+    src: 'song5',
+    bg: 'images/images.png',
+    name: 'Jamna Paar Tony Kakkar'
+  },
+  {
+    src: 'song6',
+    bg: 'images/images.png',
+    name: 'Nasha Sukhee '
+  },
+  {
+    src: 'song7',
+    bg: 'images/images.png',
+    name: 'baap to baap rhega'
+  },
+  {
+    src: 'song8',
+    bg: 'images/images.png',
+    name: 'you are very chalak bro'
+  },
+  {
+    src: 'song9',
+    bg: 'images/images.png',
+    name: 'Tu Aashiqui Hai Meri Payal Dev'
+  }
+  ]
+  
+  
+let allStories = [
+  {
+    src: 'video1'
+  }
+  ]
